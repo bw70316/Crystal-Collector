13e8cb345e411c73cee341c157ad061b41fbd034
brian wisnowski <brian wisnowski>
2017-03-15 18:38:12 -0400

    just need to add html

---
+++ dfsdsdf.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+   $(function() {
+        totalScore = $(".randomNumber").html(Math.floor(Math.random() * 60) + 1);
+   console.log(totalScore);
+
+
+
+	
+
+	$(".diamond").click(function() {
+		userGuess = $(".score").html(Math.ceil(Math.random() * 12))+1;
+
+			$("#ruby").click(function() {
+		userGuess = $(".score").html(Math.ceil(Math.random() * 6))+1;
+		
+		$("#gems").click(functi.html(Math.ceil(Math.random() * 6))+1;on() {
+		guess = $(".score")
+
+
+
+if (userGuess!==totalScore) {
+
+
+
+}
+
+	});
+
+		});
+	});
+
+ });
+
+
+});
